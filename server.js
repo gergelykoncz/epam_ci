19411cad3f5935754c0fda64abb8907b901973e1
gkoncz <gergo.koncz@gmail.com>
2016-04-20 11:23:06 +0200

    Test commit

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log("Starting server..."); 
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname));
+
+app.listen(3000, function () {
+   console.log('Starting server on port 3000');
+});
